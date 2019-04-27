bca905be2a33d4eb22a8f91dc219fcf289adbaf9
Adam Doupe <adamdoupe@gmail.com>
2019-04-27 19:40:43 +0000

    Fix the check script by making nc wait a bit

--- interaction/check.sh
+++ interaction/check.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
 
 
-RESULT=$(echo "adam" | nc "$1" "$2")
+RESULT=$(echo "adam" | nc -q 1 "$1" "$2")
 echo "$RESULT" | grep "How good are you around the corners?"
 echo "$RESULT" | grep "Send me your ride"
 echo "$RESULT" | grep "You ain't ready."
 
 
-RESULT=$(python -c "print('a'*25 + '\x00')" | nc "$1" "$2")
+RESULT=$(python -c "print('a'*25 + '\x00')" | nc -q 1 "$1" "$2")
 echo "$RESULT" | grep "You're not up to code."
